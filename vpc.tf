343e5b49a8a90bf359c8a33771b1ade2e8c35ca0
suresh <suresh@gmail.com>
2019-11-07 07:44:20 +0530

    ssh demo

--- vpc.tf
+++ vpc.tf
@@ -5,3 +5,12 @@ resource "aws_vpc" "myvpc" {
     Name = "${var.vpc_name}"
   }
 }
+
+# resource "aws_vpc" "myvpc" {
+#   cidr_block = "${var.vpc_cidir}"
+#
+#   tags = {
+#     Name = "${var.vpc_name}"
+#   }
+# }
+
